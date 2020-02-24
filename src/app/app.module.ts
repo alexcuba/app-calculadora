4b501c2fa11f95a411d0f2c0345748223a331e2e
Alexander <avmolinapro@gmail.com>
2020-02-24 16:24:53 +0100

    creamos el componete formulario para la calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
